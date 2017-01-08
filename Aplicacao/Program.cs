a0ba90f3e4edce70344022e390f257512578285e
Luís Gabriel Nascimento Simas <gabrielsimas@gmail.com>
2017-01-08 18:54:19 -0200

    Camada de Aplicação
    Camada de Aplicação Vazia

---
+++ Aplicacao/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aplicacao
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
