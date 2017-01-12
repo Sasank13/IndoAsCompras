b38c9e65bb586b14b6574cb2ef505a460af95577
Luís Gabriel Nascimento Simas <gabrielsimas@gmail.com>
2017-01-12 08:12:37 -0200

    Criação de contrato da Camada de Domínio com a Camada de Infraestrutura
    Um Domínio só pode acessar uma camada de infraestrutura através de
    contratos e assim acessar, via Façade, a camada inferior. Criados os
    contratos para os repositórios da entidades envolvidas.

---
+++ Dominio/Contratos/Repositorio/ICategoria.cs
@@ -0,0 +1,10 @@
+﻿using Dominio.Entidades;
+using Infraestrutura.Repositorios.Interfaces.Comum;
+
+namespace Dominio.Contratos.Repositorio
+{
+    public interface ICategoria:IBase<Categoria>
+    {
+
+    }
+}
---
+++ Dominio/Contratos/Repositorio/IUnidade.cs
@@ -0,0 +1,14 @@
+﻿using Dominio.Entidades;
+using Infraestrutura.Repositorios.Interfaces.Comum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dominio.Contratos.Repositorio
+{
+    public interface IUnidade: IBase<Unidade>
+    {
+
+    }
+}
